814e7a805f4e6a19835c3e6370557fcb9bb05280
Joel Williams <audio.sauce@gmail.com>
2025-07-28 21:48:46 -0700

    Change sizes and font

--- generate_envelopes.py
+++ generate_envelopes.py
@@ -38,7 +38,7 @@ RETURN_ADDRESS_LINES = [
     RETURN_ADDRESS_CITY_STATE_ZIP
 ]
 
-RETURN_ADDRESS_FONT = "Trebuchet MS"
+RETURN_ADDRESS_FONT = "Helvetica-Bold"
 RETURN_ADDRESS_SIZE = 14
 RETURN_ADDRESS_LINE_HEIGHT = 14
 RETURN_ADDRESS_X = 10  # points from left edge
@@ -46,12 +46,12 @@ RETURN_ADDRESS_Y_START = 270  # points from top (297 - 275 = 22 pts margin)
 
 # Optional image next to return address
 LOGO_PATH = "evil-wizard-logo.png"  # replace with your image path
-LOGO_SIZE = 32  # height in points; image will scale to match this
+LOGO_SIZE = 36  # height in points; image will scale to match this
 
 def draw_envelope(c, first_name, last_name, address1, address2, city, state, postal_code):
-    line_height = 14
-    font_name = "Helvetica"
-    font_size = 16
+    line_height = 18
+    font_name = "Helvetica-Bold"
+    font_size = 18
 
     # Return address
     c.setFont(RETURN_ADDRESS_FONT, RETURN_ADDRESS_SIZE)
